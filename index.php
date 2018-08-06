aedbe4930486df3f6e72b5c42f6861a4cda2a684
Unknown <krn-oviedo@hotmail.com>
2018-08-06 10:34:49 -0500

    Creación del index y menú principal

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Index</title>
+  </head>
+  <body>
+
+    <p>Página de inicio</p>
+    <br><br>
+
+    <a href="menu.php">Ir al menú</a>
+
+  </body>
+</html>
---
+++ menu.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Menu</title>
+  </head>
+  <body>
+
+    <p>Menú</p>
+    <br><br>
+
+    <a href="#">Introducción</a><br>
+    <a href="#">Aplicaciones y ventajas</a><br>
+    <a href="#">Interacción</a><br>
+    <a href="#">Acerca de...</a><br>
+    <a href="#">Contenido</a><br><br>
+
+    <a href="index.php">Volver al inicio</a>
+
+  </body>
+</html>
